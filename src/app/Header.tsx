b927dd4f92f66913daaf6412b59737ce27f8016a
popomon <popomonclub@gmail.com>
2024-05-22 10:54:17 +0900

    refactor: 헤더/푸터 컴포넌트 추가

---
+++ src/app/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<header>
+			<h1>My Application</h1>
+		</header>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import Header from './Header';
+import Footer from './Footer';
 
 export default function Home() {
 	return (
-		<main>
-			<h1>안녕!</h1>
-		</main>
+		<div>
+			<Header />
+			<main>
+				<h1>안녕!</h1>
+			</main>
+			<Footer />
+		</div>
 	);
 }
