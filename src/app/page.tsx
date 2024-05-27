45cdbd9f5a59f592eb0614cb511781b8b1bd598b
popomon <popomonclub@gmail.com>
2024-05-28 01:37:32 +0900

    refactor: 컴포넌트 폴더 추가 및 헤더/푸터 파일 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image';
 import styles from './page.module.css';
-import Header from './Header';
-import Footer from './Footer';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default function Home() {
 	return (
-		<div>
+		<>
 			<Header />
 			<main>
 				<h1>안녕!</h1>
 			</main>
 			<Footer />
-		</div>
+		</>
 	);
 }
